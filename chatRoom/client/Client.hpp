b891a3f4e562765345df88e319e9cc8cea1bb66c
nandy1211 <nandhakishore1211@gmail.com>
2025-12-19 01:31:06 +0530

    Changes done

---
+++ chatRoom/client/Client.hpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string>
+#include <algorithm>
+
+namespace Client
+{
+    class Tcp
+    {
+    private:
+        uint16_t _clientSockFd;
+        uint16_t _port;
+        sockaddr_in _clientSockAddr;
+        size_t _msgLen;
+        std::string _msg;
+
+    public:
+        explicit Tcp(uint16_t p_port);
+        void prepareSocketAndConnectToServer();
+        void recvMsg();
+        static ssize_t recv_all(int fd, void *buf, size_t len);
+        int recvMsgLength();
+        void sendMsgToServer();
+        bool sendMessage(const std::string &msg);
+        bool receiveMessage(std::string &outMsg);
+        void closeConnection();
+        void chatLoop();
+    };
+}
